2e353ec0af6cd9786cf9cba72e6c64a77f2472d5
vpidkivka <vpidkivka@eliteit.org>
2020-04-09 16:40:41 +0300

    Updates

---
+++ precache-manifest.264975f2761583807d8ce704389f1e86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b266b6982106c4ba81af075a30f377fb",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f3517ccb9e8b16d096cd",
+    "url": "/portfolio/static/css/main.636f9a2c.chunk.css"
+  },
+  {
+    "revision": "1f7514058b6976df5b70",
+    "url": "/portfolio/static/js/2.554497e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.554497e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3517ccb9e8b16d096cd",
+    "url": "/portfolio/static/js/main.a6fc4181.chunk.js"
+  },
+  {
+    "revision": "1e74db4740f99c68a0bf",
+    "url": "/portfolio/static/js/runtime-main.b37861ed.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.0f5f94e58e74ad410b451fc959252dcb.js"
+  "/portfolio/precache-manifest.264975f2761583807d8ce704389f1e86.js"
 );
 
 self.addEventListener('message', (event) => {
