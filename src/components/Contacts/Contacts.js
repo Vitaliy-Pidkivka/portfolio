029b25eb51619d7b644ae53d8a785bbb527a61ec
vpidkivka <vpidkivka@eliteit.org>
2020-04-09 23:00:19 +0300

    Stylized contacts section

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -7,16 +7,14 @@ function Contacts() {
     return (
         <div className={styles['contacts']}>
             <div className="container">
-                <div className="contacts__inner">
+                <div className={styles.contacts__inner}>
                     <Title title="Contacts"/>
-                    <div className={styles.contacts__form}>
-                        <form action="#" method="Post" className={styles.form}>
-                            <input type="text" placeholder="placeholder" required/>
-                            <input type="text" placeholder="placeholder" required/>
-                            <input type="text" placeholder="placeholder"/>
-                            <input type="submit" value="Send"/>
-                        </form>
-                    </div>
+                    <form action="#" method="Post" className={styles.form}>
+                        <input type="text" placeholder="Name" required/>
+                        <input type="email" placeholder="Email" required/>
+                        <textarea type="text" placeholder="Message"/>
+                        <input type="submit" value="Send"/>
+                    </form>
                 </div>
             </div>
         </div>
