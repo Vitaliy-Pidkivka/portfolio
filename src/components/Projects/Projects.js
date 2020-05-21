54b2b7c92630b2e71b18945b6446814919d90f4f
vpidkivka <vpidkivka@eliteit.org>
2020-05-21 18:45:31 +0300

    add new project (MoGo)

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,43 +4,55 @@ import Title from "../Shared/Title/Title";
 import Project from "./Project/Project";
 import todoList from '../../assets/images/projects/todo-list.png'
 import calculator from '../../assets/images/projects/calculator.png'
+import mogo from '../../assets/images/projects/mogo.png'
 import zebra from '../../assets/images/projects/zebra.png'
 import counter from '../../assets/images/projects/counter.png'
 
 const state = {
     projects: [
 
-        {projectLink: 'https://github.com/Vitaliy-Pidkivka/social-network',
+        {
+            projectLink: 'https://github.com/Vitaliy-Pidkivka/social-network',
             image: 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg',
             project: 'Social network',
             description: 'React SPA',
             id: 1,
         },
-        {projectLink: 'https://vitaliy-pidkivka.github.io/todo-list/',
+        {
+            projectLink: 'https://vitaliy-pidkivka.github.io/todo-list/',
             image: todoList,
             project: 'Todo list',
             description: 'React application',
             id: 2,
         },
-        {projectLink: 'https://vitaliy-pidkivka.github.io/counter/',
+        {
+            projectLink: 'https://vitaliy-pidkivka.github.io/counter/',
             image: counter,
             project: 'Counter',
             description: 'React application',
             id: 3,
         },
-        {projectLink: 'https://vitaliy-pidkivka.github.io/Calculator/',
+        {
+            projectLink: 'https://vitaliy-pidkivka.github.io/Calculator/',
             image: calculator,
             project: 'Calculator',
             description: 'Bootstrap, JS, CSS application',
             id: 4,
         },
-        {projectLink: 'https://vitaliy-pidkivka.github.io/Zebra/',
-            image: zebra,
-            project: 'Zebra',
+        {
+            projectLink: 'https://vitaliy-pidkivka.github.io/MoGo/',
+            image: mogo,
+            project: 'Mogo',
             description: 'Landing page',
             id: 5,
         },
-
+        {
+            projectLink: 'https://vitaliy-pidkivka.github.io/Zebra/',
+            image: zebra,
+            project: 'Zebra',
+            description: 'Landing page',
+            id: 6,
+        },
     ]
 }
 
